cabd662ad0fdf3066753ed5d27c632ca86da42ef
Igor Chernyshev <sonic96.ic@gmail.com>
2026-07-09 17:40:09 +0500

    fix padding for fullscreen telegram app

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css'
 import '@mantine/dates/styles.css'
 import '@mantine/notifications/styles.css'
+import './global.css'
 import 'dayjs/locale/ru'
 
 import { MantineProvider } from '@mantine/core'
